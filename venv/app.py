545215cf5855c9d691c6ff7d0e1fe8914d36db45
Praveen <pravi.krishna@gmail.com>
2024-08-16 12:59:56 +0530

    Update app.py

--- venv/app.py
+++ venv/app.py
@@ -3,19 +3,10 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
-client = OpenAI(
-    base_url="https://integrate.api.nvidia.com/v1",
-    api_key="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"
-)
+import streamlit as st
+st.title("🦜🔗 Praveen build his first App")
 
-completion = client.chat.completions.create(
-    model="meta/llama-3.1-8b-instruct",
-    messages=[{"role": "user", "content": "list the SUV cars in India."}],
-    temperature=0.2,
-    top_p=0.7,
-    max_tokens=1024,
-    stream=True
-)
+
 
 # for chunk in completion:
 #  if chunk.choices[0].delta.content is not None:
@@ -31,25 +22,27 @@ client = ChatNVIDIA(
 prompt = ChatPromptTemplate.from_messages(
     [("system", "You are a helpful AI assistant named Fred."), ("user", "{input}")]
 )
+def generate_response(input_text):
+
+    chain = prompt | client | StrOutputParser()
+    context = ""
+    for txt in chain.stream({"input": input_text}):
+        context += str(txt)
+    st.info(context)
+
 
 
 def handle_conversion():
-    context = ""
-    print("Welcome to the Chatbot. enter 'exit' to exit the chatbot")
-    while True:
-        user_input = input("\nYou: ")
-        if user_input == "exit":
-            break
-        chain = prompt | client | StrOutputParser()
 
-        for txt in chain.stream({"input": user_input}):
-            context += f"\nUser: {user_input}\n AI :{str(txt)}"
-            print(txt, end="")
+    with st.form("my_form"):
+        text = st.text_area(
+            "Enter text:",
+            "Your query here!!!",
+        )
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            generate_response(text)
 
 
 if __name__ == "__main__":
     handle_conversion()
-
-
-
-
